599e83759501e1a4c6d09d38996ed90dc0dbfb50
Bora Berk <87586166+boraberkcetintas@users.noreply.github.com>
2022-10-11 00:25:20 +0300

    New Game Settings added

--- Assets/Scripts/GameSettings.cs
+++ Assets/Scripts/GameSettings.cs
@@ -8,10 +8,25 @@ public class GameSettings : ScriptableObject
 {
     public int SafeZoneLevel;
     public int SuperZoneLevel;
-    public int MaxContentCountInWheel;
+    public int MaxContentCountInWheel = 8; //Must be a fixed number
+    public float MoveInCanvasStopLimit;
+    public float MoveInCanvasLerp;
+
     [Header("Wheel Settings")]
     [Range(1, 15)] public int SpinDuration;
     public int WheelTurnCount;
     public float RewardPlaceHolderOffset;
+    public float WaitingTimeAfterSpinCompleted;
+    public DG.Tweening.Ease SpinningType;
+
+    [Header("UI Settings")]
+    public Vector2 WheelBombSpriteSize;
+    public Vector2 WheelOtherSpriteSize;
+    public float SpacingForEarnedPrizeBoard = -110f;
+
+    [Header("Sound Settings")]
+    public AudioClip TickAudioClip;
+    [Range(0f, 1f)] public float Volume = .5f;
+    [Range(-3f, 3f)] public float Pitch = 1f;
 
 }
